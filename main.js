6a5f6cd164f0dee9c7089e27a4a73ad9b9639969
AlicePisciuneri <alice.corleto@libero.it>
2025-07-26 16:08:37 +0200

    bici corretto

--- main.js
+++ main.js
@@ -1,33 +1,20 @@
-//Snack 1 Creare un array di oggetti:
-//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
-//Stampare a schermo la bici con peso minore.
 
-const bici1 = {
-    nome: 'Bartali',
-    peso:  3
-}
-const bici4= {
-    nome: 'Pantani',
-    peso:  0
-};
-const bici2 = {
-    nome: 'Coppi',
-    peso:  2
-};
-const bici3= {
-    nome: 'Nibali',
-    peso:  1
-};
 
-const bici = [bici1,bici2,bici3,bici4];
 
-let biciLeggera = bici[0]; 
+const bici = [
+  { nome: 'Bartali', peso: 3 },
+  { nome: 'Coppi', peso: 2 },
+  { nome: 'Nibali', peso: 1 },
+  { nome: 'Pantani', peso: 0 }
+];
 
-for (let i = 1; i < bici.length; i++) {
-    if (bici[i].peso < biciLeggera.peso) {
-        biciLeggera = bici[i];
-    }
+ //somma iniziale
+let pesoTotale = 0;
+
+// 2. Ciclo sull’array
+for (let i = 0; i < bici.length; i++) {
+  pesoTotale += bici[i].peso; // aggiungo il peso della bici attuale
 }
 
-console.log(`La bici più leggera è ${biciLeggera.nome} con peso ${biciLeggera.peso} kg`);
-
+// 3. Stampo il risultato
+console.log(`Il peso totale di tutte le bici è ${pesoTotale} kg`);
